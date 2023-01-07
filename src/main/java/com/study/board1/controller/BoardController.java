c5b157de93ac1ca77d3c40e04070ed850c02999c
khoney0929 <khoney0929@gmail.com>
2023-01-08 02:06:06 +0900

    3rd commit / selfmade-board / by file input

--- src/main/java/com/study/board1/controller/BoardController.java
+++ src/main/java/com/study/board1/controller/BoardController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @Controller
@@ -21,10 +23,24 @@ public class BoardController {
         return "boardwrite";
     }
     @PostMapping("/boardwritepro")
-    public String boardWritePro(Board board){
+    public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception{
 
-        boardService.write(board);
-        return "";
+        boardService.write(board, file);
+
+        model.addAttribute("message","글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/boardlist");
+
+
+        return "message";
+    }
+
+    @GetMapping("/boardmodify/{id}")
+    public String boardModify(@PathVariable("id") Integer id,
+                              Model model){
+        model.addAttribute("board", boardService.boardView(id));
+
+
+        return "boardmodify";
     }
 
     @GetMapping("/boardlist")
@@ -53,6 +69,19 @@ public class BoardController {
 
     }
 
+
+    @PostMapping("/boardupdate/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file) throws Exception{
+
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp, file);
+
+
+        return "redirect:/boardlist";
+    }
 
 
 }
--- src/main/java/com/study/board1/service/BoardService.java
+++ src/main/java/com/study/board1/service/BoardService.java
@@ -4,8 +4,11 @@ import com.study.board1.entity.Board;
 import com.study.board1.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardService {
@@ -15,7 +18,20 @@ public class BoardService {
 
 
     //글 작성 처리
-    public void write(Board board){
+    public void write(Board board, MultipartFile file) throws Exception{
+
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+
+        UUID uuid = UUID.randomUUID();
+
+        String fileName = uuid + "_" + file.getOriginalFilename();
+
+        File saveFile = new File(projectPath, fileName);
+
+        file.transferTo(saveFile);
+
+        board.setFilename(fileName);
+        board.setFilepath("/files/"+fileName);
 
         boardRepository.save(board);
 
